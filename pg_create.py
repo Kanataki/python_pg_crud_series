3865c9a9fc7a54a7643a7664945819f20f5dcd96
Kanataki <gichiniwaruingi@gmail.com>
2019-03-25 15:33:03 +0300

    creating pg table with python - initial commit

---
+++ pg_create.py
@@ -0,0 +1,29 @@
+import psycopg2
+try:
+    connection = psycopg2.connect(user = "postgres",
+                                  password = "xxxx",
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "crudseries")
+    cursor = connection.cursor()
+    #define Create Query
+    create = '''create table stage1.cake_flavours
+                (
+                  cake_id serial not null,
+                  cake_type varchar(30),
+                  tasty_meter varchar(30),
+                  primary key (cake_id)
+                );'''
+
+    #Execute create
+    cursor.execute(create)
+    connection.commit()
+    print("Table created successfully!")
+except (Exception, psycopg2.Error) as error :
+    print ("Error while attempting to Create Table :-(", error)
+finally:
+    #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
